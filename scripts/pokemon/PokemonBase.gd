a7a3dbfb475ea0d713ba253401c9dad0a44bfed3
sdvanhoorne <52832921+sdvanhoorne@users.noreply.github.com>
2025-07-19 10:58:13 -0400

    adjusting pokemon classes

--- scripts/pokemon/PokemonBase.gd
+++ scripts/pokemon/PokemonBase.gd
@@ -4,10 +4,13 @@ class_name PokemonBase
 
 var id: int
 var name: String
-var base_stats: Dictionary # { "hp": int, "attack": int, ... }
-var learnable_moves: Array[Dictionary] # [ { "level": int, "move_id": int } ]
-var evolutions: Array[Dictionary] # [ { "level": int, "pokemon_id": int } ]
+var base_stats = null
+var learnable_moves: Array[Dictionary] 
+var evolutions: Array[Dictionary]
 
 func _init(data := {}):
 	id = data["id"]
 	name = data["name"]
+	base_stats = PokemonStats.new(data["base_stats"])
+	learnable_moves = data["learnable_moves"]
+	evolutions = data["evolutions"]
