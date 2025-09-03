cb3b3addc11fa1e330643e90369a152f81f17ae0
sdvanhoorne <52832921+sdvanhoorne@users.noreply.github.com>
2025-09-02 20:25:24 -0400

    tooltip

--- scripts/battle/PokemonTooltip.gd
+++ scripts/battle/PokemonTooltip.gd
@@ -1,11 +1,18 @@
 extends Node2D
 @onready var HoverTooltip = $PopUpStats
+@onready var SpriteArea = $SpriteArea
+
+func _ready() -> void:
+	SpriteArea.mouse_entered.connect(_on_sprite_area_mouse_entered)
+	SpriteArea.mouse_exited.connect(_on_sprite_area_mouse_exited)
+	HoverTooltip.visible = false
 
 func _process(_delta):
 	if HoverTooltip.visible:
 		HoverTooltip.global_position = get_viewport().get_mouse_position()
 
 func _on_sprite_area_mouse_entered() -> void:
+	HoverTooltip.visible = true
 	var pokemon = get_meta("pokemon")
 	var container = HoverTooltip.get_node("VBoxContainer")
 	container.get_node("Attack").text = "Attack: %d" % pokemon.battle_stats.attack
@@ -13,7 +20,6 @@ func _on_sprite_area_mouse_entered() -> void:
 	container.get_node("SpecialAttack").text = "SpecialAttack: %d" % pokemon.battle_stats.special_attack
 	container.get_node("SpecialDefense").text = "SpecialDefense: %d" % pokemon.battle_stats.special_defense
 	container.get_node("Speed").text = "Speed: %d" % pokemon.battle_stats.speed
-	HoverTooltip.visible = true
 
 func _on_sprite_area_mouse_exited() -> void:
 	HoverTooltip.visible = false
